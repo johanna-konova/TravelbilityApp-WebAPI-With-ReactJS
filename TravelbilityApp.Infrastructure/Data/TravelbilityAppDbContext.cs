b4547449cff6314a2e0fe6ee6d5907e65e275f41
johanna-konova <j.konova@abv.bg>
2025-05-30 16:37:25 +0300

    add ApplicationUser, ApplicationRole and TravelbilityAppDbContext

---
+++ TravelbilityApp.Infrastructure/Data/TravelbilityAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TravelbilityApp.Infrastructure.Data.Models;
+
+namespace TravelbilityApp.Infrastructure.Data
+{
+    public class TravelbilityAppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public TravelbilityAppDbContext(DbContextOptions<TravelbilityAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
