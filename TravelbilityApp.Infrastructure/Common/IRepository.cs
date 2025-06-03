49a91ce9a0f7bf9a7c9c584261456a2b720f9af8
johanna-konova <j.konova@abv.bg>
2025-06-03 11:05:12 +0300

    add and implement repository

---
+++ TravelbilityApp.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelbilityApp.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+        IQueryable<T> AllAsNoTracking<T>() where T : class;
+        Task<T?> GetByIdAsync<T>(Guid id) where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task<int> SaveChangesAsync();
+    }
+}
