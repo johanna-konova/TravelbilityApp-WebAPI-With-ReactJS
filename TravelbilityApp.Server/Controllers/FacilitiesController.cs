1144b5f84b759f045c8106d3be8367d3a2b3c500
johanna-konova <j.konova@abv.bg>
2025-06-03 11:15:20 +0300

    add and implement property types and facilities controllers

---
+++ TravelbilityApp.Server/Controllers/FacilitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TravelbilityApp.Core.Contracts;
+using TravelbilityApp.Core.DTOs.Property;
+
+namespace TravelbilityApp.WebAPI.Controllers
+{
+    [Authorize]
+    public class FacilitiesController(IFacilityService facilityService) : BaseController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [ProducesResponseType(typeof(PropertyFacilityOptionDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult> Get()
+        {
+            var propertyFacilities = await facilityService.GetAllAsync();
+
+            return Ok(propertyFacilities);
+        }
+    }
+}
---
+++ TravelbilityApp.Server/Controllers/PropertyTypesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TravelbilityApp.Core.Contracts;
+using TravelbilityApp.Core.DTOs.Property;
+
+namespace TravelbilityApp.WebAPI.Controllers
+{
+    [Authorize]
+    public class PropertyTypesController(IPropertyTypeService propertyTypeService) : BaseController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [ProducesResponseType(typeof(PropertyTypeOptionDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetAll()
+        {
+            var propertyTypes = await propertyTypeService.GetAllAsync();
+
+            return Ok(propertyTypes);
+        }
+    }
+}
