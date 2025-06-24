02b13a703b2d0970c88fe464b5d25f842c136139
johanna-konova <j.konova@abv.bg>
2025-06-24 12:55:38 +0300

    add filters / attributes

---
+++ TravelbilityApp.Server/Filters/ExistingPropertyAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using TravelbilityApp.Core.Contracts;
+
+namespace TravelbilityApp.WebAPI.Filters
+{
+    public class ExistingPropertyAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.ActionArguments.TryGetValue("id", out var rowId) == false ||
+                rowId is not Guid propertyId)
+            {
+                context.Result = new BadRequestResult();
+                return;
+            }
+
+            IPropertyService? propertyService =
+                context.HttpContext.RequestServices.GetService<IPropertyService>();
+
+            if (propertyService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                return;
+            }
+
+            var hasPropertyWithGivenId = await propertyService.HasPropertyWithGivenIdAsync(propertyId);
+
+            if (hasPropertyWithGivenId == false)
+            {
+                context.Result = new NotFoundResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ TravelbilityApp.Server/Filters/PropertyPublisherAttribute.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using System.Security.Claims;
+
+using TravelbilityApp.Core.Contracts;
+
+namespace TravelbilityApp.WebAPI.Filters
+{
+    public class PropertyPublisherAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (context.ActionArguments.TryGetValue("id", out var rowId) == false ||
+                rowId is not Guid propertyId)
+            {
+                context.Result = new BadRequestResult();
+                return;
+            }
+
+            IPropertyService? propertyService =
+                context.HttpContext.RequestServices.GetService<IPropertyService>();
+
+            if (propertyService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                return;
+            }
+
+            var userId = context.HttpContext.User.Id();
+
+            var isUserPropertyPublisher = await propertyService.IsUserPropertyPublisherAsync(propertyId, userId);
+
+            if (isUserPropertyPublisher == false)
+            {
+                context.Result = new ForbidResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
