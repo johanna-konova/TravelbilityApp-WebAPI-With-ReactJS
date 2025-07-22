5437a6eaaa95c7ca2a33e88810b5979b994b93b8
johanna-konova <j.konova@abv.bg>
2025-07-22 13:47:29 +0300

    update / expand some properties components and css styles

--- travelbilityapp.client/src/components/properties/user-properties/Property-Short-Grid-View.jsx
+++ travelbilityapp.client/src/components/properties/user-properties/Property-Short-Grid-View.jsx
@@ -8,29 +8,45 @@ import UserActions from '../../user-actions/User-Actions';
 import styles from './User-Properties.module.css';
 
 export default function PropertyShortGridView({ propertyData }) {
+    const isPublished = propertyData.status === "Published";
+
     return (
-        <>
-            <Card className={styles["property-card"]}>
+        <Card className={styles["property-card"]}>
+            <div className="position-relative">
+                <div
+                    className={styles["status-label"]}
+                    style={{ backgroundColor: isPublished ? "#7AB730" : "#c82333" }}
+                >
+                    {isPublished ? "Published" : "Unpublished"}
+                </div>
                 <Link to={`/properties/${propertyData.id}`}>
                     <Card.Img variant="top" src={propertyData.mainPhotoUrl} />
                 </Link>
-                <Card.Body className="pb-0">
-                    <Link to={`/properties/${propertyData.id}`}>
-                        <Card.Title className="mb-0">{propertyData.name}</Card.Title>
-                    </Link>
-                    <div className={styles["stars"]}>{generateStarIcons(Number(propertyData.starsCount))}</div>
-                    <Card.Text className="mt-2">
-                        <i className="fas fa-map-marker-alt"></i> <span>{propertyData.address}</span>
-                    </Card.Text>
-                    <Card.Text>{propertyData.description.slice(0, 150) + '...'}</Card.Text>
-                </Card.Body>
-                
+            </div>
+            <Card.Body className="pb-0">
+                <Link to={`/properties/${propertyData.id}`}>
+                    <Card.Title className="mb-0">{propertyData.name}</Card.Title>
+                </Link>
+                <div className={styles["stars"]}>{generateStarIcons(Number(propertyData.starsCount))}</div>
+                <Card.Text className="mt-2">
+                    <i className="fas fa-map-marker-alt"></i> <span>{propertyData.address}</span>
+                </Card.Text>
+                <Card.Text>{propertyData.description.slice(0, 150) + '...'}</Card.Text>
+            </Card.Body>
+
+            {isPublished === false &&
+                <Link className={styles["hasNoAccessibleRoom"]} to={`/my-properties/${propertyData.id}`}>*Add at least one Accessible Room to publish your property.</Link>
+            }
+
+            <div className={styles["user-actions"]}>
+                <Link to={`/my-properties/${propertyData.id}`}>
+                    <span className="ml-3 text-primary">View as publisher</span>
+                </Link>
                 <UserActions
                     id={propertyData.id}
                     name={propertyData.name}
-                    hasPaddingBottom={true}
                 />
-            </Card>
-        </>
+            </div>
+        </Card>
     )
 };
--- travelbilityapp.client/src/components/properties/user-properties/User-Properties.jsx
+++ travelbilityapp.client/src/components/properties/user-properties/User-Properties.jsx
@@ -4,7 +4,7 @@ import { useAuthContext } from '../../../contexts/Auth-Context';
 import { PropertiesContext } from '../../../contexts/Properties-Context';
 
 import { useBasicGetFetch } from '../../../hooks/use-basic-get-fetch';
-import { getByPublisherId } from '../../../services/propertiesService';
+import { getAllByPublisherId } from '../../../services/propertiesService';
 
 import NoProperties from './No-Properties';
 import UserPropertiesContainer from './User-Properties-Container';
@@ -15,7 +15,7 @@ export default function UserProperties() {
     const {
         data: propertiesDataByPublisherId,
         isDataLoaded: isPropertiesDataByPublisherIdLoaded,
-        removeDataElement: deletePropertyByIdHandler } = useBasicGetFetch(() => getByPublisherId(id));
+        removeDataElement: deletePropertyByIdHandler } = useBasicGetFetch(() => getAllByPublisherId(id));
 
     return (
         <>
