e6f005b59db83761c0e0d2c2fbf66a4d1ab09331
johanna-konova <j.konova@abv.bg>
2025-07-22 13:43:48 +0300

    add room validation scheme

--- travelbilityapp.client/src/validations.js
+++ travelbilityapp.client/src/validations.js
@@ -1,4 +1,4 @@
-import * as yup from "yup";
+﻿import * as yup from "yup";
 
 export const registrationSchema = yup.object().shape({
     email: yup
@@ -70,6 +70,63 @@ export const propertySchema = yup.object().shape({
         accessibilityIds: yup
             .array()
             .min(1, "Please, select at least 1 Accessibility."),
+    }),
+
+    "step-3": yup.object({
+        photoUrls: yup
+            .array()
+            .min(5, "Please, upload at least 5 Photos with a valid URL format."),
+    }),
+});
+
+export const roomSchema = yup.object().shape({
+    "step-1": yup.object({
+        roomTypeId: yup
+            .string()
+            .typeError("Please, select a Type.")
+            .required("Please, select a Type."),
+
+        maxGuests: yup
+            .number()
+            .typeError("Please, select Maximum count of guests.")
+            .required("Please, select Maximum count of guests."),
+
+        pricePerNight: yup
+            .number()
+            .typeError("Please, enter a valid Price per night.")
+            .required("Please, enter a valid Price per night.")
+            .min(0.01, "The Price per night must be at least 0.01 EUR."),
+
+        mainBedTypeId: yup
+            .number()
+            .typeError("Please, select a Main bed type.")
+            .required("Please, select a Main bed type."),
+
+        size: yup
+            .number()
+            .typeError("Please, enter a valid Room size.")
+            .required("Please, enter a valid Room size.")
+            .min(5, "The Room size must be at least 5 m².")
+            .max(5000, "The Room size must be at most 5000 m²."),
+
+        numberOfUnits: yup
+            .number()
+            .typeError("Please, enter a valid Number of Rooms.")
+            .required("Please, enter a valid Number of Rooms.")
+            .min(1, "The Number of Rooms must be at least 1.")
+            .max(8000, "The Number of Rooms must be at most 8000."),
+
+        description: yup
+            .string()
+            .required("Please, enter a Description.")
+            .min(10, "The Description must be at least 10 characters long.")
+            .max(1000, "The Description must be at most 1000 characters long."),
+    }),
+
+    "step-2": yup.object({
+        commonFacilityIds: yup
+            .array()
+            .min(1, "Please, select at least 1 Facility."),
     }),
 
     "step-3": yup.object({
