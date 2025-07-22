5b0e3ef51f7555b0e6c94545def19c2a4b4ec2ab
johanna-konova <j.konova@abv.bg>
2025-07-22 13:48:33 +0300

    add create / edit room component

---
+++ travelbilityapp.client/src/components/create-edit-forms/room-create-edit/Room-Basic-Information.jsx
@@ -0,0 +1,129 @@
+﻿import { Form, Row, Col } from 'react-bootstrap';
+
+export default function RoomBasicInformation({
+    roomTypes,
+    bedTypes,
+    errors,
+    register
+}) {
+    return (
+        <>
+            <h4 className="mb-3 text-center text-primary">Basic information</h4>
+
+            <Row>
+                <Col sm={4} md={4} lg={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Type</Form.Label>
+                        <Form.Select
+                            defaultValue=""
+                            className="custom-select px-4"
+                            {...register("step-1.roomTypeId")}
+                        >
+                            <option value="" disabled>--- Select ---</option>
+                            {roomTypes.map(rt => (
+                                <option key={rt.id} value={rt.id}>{rt.name}</option>
+                            ))}
+                        </Form.Select>
+                        {errors?.roomTypeId
+                            ? <p className="text-danger">{errors?.roomTypeId.message}</p>
+                            : (errors?.RoomTypeId && errors.RoomTypeId.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                        }
+                    </Form.Group>
+                </Col>
+
+                <Col sm={4} md={4} lg={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Maximum guest count</Form.Label>
+                        <Form.Select
+                            defaultValue=""
+                            className="custom-select px-4"
+                            {...register("step-1.maxGuests")}
+                        >
+                            <option value="" disabled>--- Select ---</option>
+                            {[1, 2, 3, 4, 5, 6].map(g => (
+                                <option key={g} value={g}>
+                                    {`${g} guest${g > 1 ? "s" : ""}`}
+                                </option>
+                            ))}
+                        </Form.Select>
+                        {errors?.maxGuests
+                            ? <p className="text-danger">{errors?.maxGuests.message}</p>
+                            : (errors?.MaxGuests && errors.MaxGuests.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                        }
+                    </Form.Group>
+                </Col>
+
+                <Col sm={4} md={4} lg={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Price per night</Form.Label>
+                        <Form.Control
+                            placeholder="ex. 00.00"
+                            {...register("step-1.pricePerNight")} />
+                            {errors?.pricePerNight
+                            ? <p className="text-danger">{errors?.pricePerNight.message}</p>
+                            : (errors?.PricePerNight && errors.PricePerNight.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                            }
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col sm={4} md={4} lg={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Main bed type</Form.Label>
+                        <Form.Select
+                            defaultValue=""
+                            className="custom-select px-4"
+                            {...register("step-1.mainBedTypeId")}
+                        >
+                            <option value="" disabled>--- Select ---</option>
+                            {bedTypes.map(bt => (
+                                <option key={bt.id} value={bt.id}>{bt.name}</option>
+                            ))}
+                        </Form.Select>
+                        {errors?.mainBedTypeId
+                            ? <p className="text-danger">{errors?.mainBedTypeId.message}</p>
+                            : (errors?.MainBedTypeId && errors.MainBedTypeId.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                        }
+                    </Form.Group>
+                </Col>
+
+                <Col sm={4} md={4} lg={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Room size (м²)</Form.Label>
+                        <Form.Control
+                            placeholder="ex. 5"
+                            {...register("step-1.size")} />
+                        {errors?.size
+                            ? <p className="text-danger">{errors?.size.message}</p>
+                            : (errors?.Size && errors.Size.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                        }
+                    </Form.Group>
+                </Col>
+
+                <Col sm={4} md={4} lg={4}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Number of Rooms of this type</Form.Label>
+                        <Form.Control
+                            type="number"
+                            min="1"
+                            {...register("step-1.numberOfUnits")} />
+                        {errors?.numberOfUnits
+                            ? <p className="text-danger">{errors?.numberOfUnits.message}</p>
+                            : (errors?.NumberOfUnits && errors.NumberOfUnits.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                        }
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Form.Group className="mb-3">
+                <Form.Label>Description</Form.Label>
+                <Form.Control as="textarea" rows={7} {...register("step-1.description")} />
+                {errors?.description
+                    ? <p className="text-danger">{errors?.description.message}</p>
+                    : (errors?.Description && errors.Description.map((message, index) => <div key={index} className="text-danger">{message}</div>))
+                }
+            </Form.Group>
+        </>
+    );
+}
---
+++ travelbilityapp.client/src/components/create-edit-forms/room-create-edit/Room-Create-Edit-Form-Modal.jsx
@@ -0,0 +1,202 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useEffect, useState } from 'react';
+import { Button, Form, Spinner } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import toast from "react-hot-toast";
+
+import { useBasicGetFetch } from '../../../hooks/use-basic-get-fetch';
+import { getAll as getBedTypes } from '../../../services/bedTypesService';
+import { getRoomFacilities } from '../../../services/facilitiesService';
+import { getAll as getRoomTypes } from '../../../services/roomTypesService';
+import { create, edit, getById } from '../../../services/roomsService';
+import { IsSelectedRoomAccessible, constructRoomDataForEditing, formatErrorsData } from '../../../utils/property-utils';
+import { roomSchema } from '../../../validations';
+import CancelModal from '../../modals/Cancel-Modal';
+import RoomBasicInformation from './Room-Basic-Information';
+import Facilities from '../common/Facilities';
+import Photos from '../common/Photos';
+
+export default function AddRoomModal({
+    roomId,
+    propertyId,
+    addRoomHandler,
+    closeModalHandler
+}) {
+    const { data: facilities } = useBasicGetFetch(() => getRoomFacilities());
+    const { data: roomTypes } = useBasicGetFetch(() => getRoomTypes());
+    const { data: bedTypes } = useBasicGetFetch(() => getBedTypes());
+
+    const [menualErrors, setMenualErrors] = useState({});
+    const [step, setStep] = useState(1);
+    const [isAccessibleRoomSelected, setIsAccessibleRoomSelected] = useState(false);
+    const [isCancelModalShowed, setIsCancelModalShowed] = useState(false);
+
+    const { control, formState: { errors, isSubmitting }, getValues, handleSubmit, register, reset, setError, setValue, trigger, watch } = useForm({
+        defaultValues: {
+            "step-2": {
+                commonFacilityIds: [],
+                accessibilityIds: []
+            },
+        },
+        mode: 'onChange',
+        resolver: yupResolver(roomSchema),
+    });
+
+    useEffect(() => {
+        if (roomId !== undefined) {
+            (async () => {
+                const roomDataToEdit = await getById(roomId, propertyId);
+                reset(constructRoomDataForEditing(roomDataToEdit));
+            }
+            )()
+        }
+    }, [roomId]);
+
+    const updateMenualErrorsHandler = (errors) => {
+        if (errors.ImageUrls) {
+            setMenualErrors(previousMenualErrors => ({
+                ...previousMenualErrors,
+                "step-3": {
+                    ...previousMenualErrors["step-3"],
+                    ImageUrls: errors.ImageUrls
+                },
+            }));
+        } else {
+            setMenualErrors(previousMenualErrors => ({ ...previousMenualErrors, ...errors }));
+        }
+    }
+
+    const hideCancelModalHandler = () => setIsCancelModalShowed(false);
+
+    const nextStep = async () => {
+        const selectedTypeId = getValues("step-1.roomTypeId");
+        const isSelectedRoomAccessible = IsSelectedRoomAccessible(roomTypes, selectedTypeId);
+
+        if (step === 2) {
+            if (isSelectedRoomAccessible === false) {
+                setValue("step-2.accessibilityIds", []);
+            } else {
+                const selectedAccessibilityIds = getValues("step-2.accessibilityIds");
+
+                /*if (selectedAccessibilityIds.length === 0) {
+                    setError("step-2.accessibilityIds", { message: "Please, select at least 1 Accessibility." });
+                    return;
+                }*/
+            }
+        }
+
+        const isStepValid = await trigger(`step-${step}`);
+
+        if (isStepValid) {
+            setIsAccessibleRoomSelected(isSelectedRoomAccessible);
+            setStep(step + 1);
+        }
+    };
+
+    const prevStep = () => setStep(step - 1);
+
+    const createHandler = async (data) => {
+        const roomDataToSave = {
+            propertyId,
+            ...data["step-1"],
+            facilityIds: [...data["step-2"].commonFacilityIds, ...data["step-2"].accessibilityIds],
+            photoUrls: data["step-3"].photoUrls.map(iu => iu.url),
+        };
+        debugger
+
+        try {
+            const savedRoomData = roomId
+                ? await edit(roomId, roomDataToSave)
+                : await create(roomDataToSave);
+
+            toast.success(`You have successfully ${roomId === undefined ? "added" : "edited"} the room.`);
+
+            addRoomHandler(savedRoomData);
+        } catch (errorInfo) {
+            const errorsData = formatErrorsData(getValues(), errorInfo.errorsData);
+
+            updateMenualErrorsHandler(errorsData);
+
+            if (Object.keys(errorsData["step-1"]).length > 0) {
+                setStep(1);
+            } else if (Object.keys(errorsData["step-2"]).length > 0) {
+                setStep(2);
+            } else if (Object.keys(errorsData["step-3"]).length > 0) {
+                setStep(3);
+            }
+        }
+    };
+
+    return (
+        <div className="custom-modal-overlay">
+            <div
+                className="custom-modal-content"
+                style={{ display: isCancelModalShowed ? "none" : "block" }}
+            >
+                <div className="x-close">
+                    <span onClick={() => setIsCancelModalShowed(true)}>X</span>
+                </div>
+                <Form onSubmit={handleSubmit(createHandler)}>
+                    {step === 1 &&
+                        <RoomBasicInformation
+                            roomTypes={roomTypes}
+                            bedTypes={bedTypes}
+                            errors={{ ...errors["step-1"], ...menualErrors["step-1"] }}
+                            register={register}
+                        />
+                    }
+
+                    {step === 2 &&
+                        <Facilities
+                            facilities={facilities}
+                            hasAccessibility={isAccessibleRoomSelected}
+                            errors={{ ...errors["step-2"], ...menualErrors["step-2"] }}
+                            register={register}
+                            watch={watch}
+                        />
+                    }
+
+                    {step === 3 &&
+                        <Photos
+                            errors={{ ...errors["step-3"], ...menualErrors["step-3"], imageUrl: menualErrors.imageUrl }}
+                            updateMenualErrorsHandler={updateMenualErrorsHandler}
+                            isDisabled={isSubmitting}
+                            control={control}
+                        />
+                    }
+
+                    <div className="d-flex justify-content-between mt-3">
+                        <div className="d-flex">
+                            {step > 1 && <Button type="button" onClick={prevStep} className="me-2">Back</Button>}
+                            <Button type="button" variant="info" onClick={() => setIsCancelModalShowed(true)}>Cancel</Button>
+                        </div>
+                        {step < 3 && <Button type="button" onClick={nextStep}>Next</Button>}
+                        {step == 3 &&
+                            < Button type="submit" disabled={isSubmitting}>
+                                {isSubmitting
+                                    ? <>
+                                        <Spinner
+                                            as="span"
+                                            animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"
+                                        />
+                                        Saving...
+                                    </>
+                                    : <span>Save</span>
+                                }
+                            </Button>
+                        }
+                    </div>
+                </Form>
+            </div>
+
+            <CancelModal
+                isModalShowed={isCancelModalShowed}
+                closeModalHandler={hideCancelModalHandler}
+                discardChangesHandler={closeModalHandler}
+            />
+        </div>
+    );
+};
